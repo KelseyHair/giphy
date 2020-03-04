93db501ebec55a2614e9e8dba73c44901bca2b08
KelseyHair <Sheliashate@gmail.com>
2020-03-04 23:25:25 +0000

    Progress

--- js/script.js
+++ js/script.js
@@ -3,9 +3,23 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-$("#search-button").click(function(){
-  
-  
-  
+var giphy = {gif:""};
+var JSON = JSON.stringify(giphy);
+//window.location = "https://giphy.com/" + JSON;
+
+$("#search-button").click(function () {
+    fetch("https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
+        .then(function () {
+            return response.json();
+        })
+        .then(function () {
+            let searchTerm  = giphy.gif;
+            $("body").append("<img src =" + searchTerm + "/>");
+            console.log("did it work");
+
+
+        })
+
+
 });
 
