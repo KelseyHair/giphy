cbba7fffec3dff497386fba5fb55fd979bca9aa1
KelseyHair <Sheliashate@gmail.com>
2020-03-18 22:29:13 +0000

    Sort of

--- js/script.js
+++ js/script.js
@@ -8,15 +8,17 @@ var JSON = JSON.stringify(giphy);
 //window.location = "https://giphy.com/" + JSON;
 
 $("#search-button").click(function () {
-    fetch("https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY")
-        .then(function () {
+    var searchT = ("#search-term").val()
+    var newUrl=`https://api.giphy.com/v1/gifs/search?q=${searchT}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`;
+    fetch(newUrl)
+    .then(function () {
             return response.json();
         })
         .then(function () {
-            let searchTerm  = giphy.gif;
-            $("body").append("<img src =" + searchTerm + "/>");
-            console.log("did it work");
-
+            //let searchTerm  = giphy.gif;
+            let pic_url = data[0].show.image.medium;
+            $('body').append('<img src =' + searchTerm + pic_url + '/>');
+            let src = " ";
 
         })
 
