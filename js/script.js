1581a057335d418a8cb0d74271321894df3d6bb0
KelseyHair <Sheliashate@gmail.com>
2020-03-25 22:36:24 +0000

    Getting there!

--- js/script.js
+++ js/script.js
@@ -3,24 +3,25 @@
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
 
-var giphy = {gif:""};
-var JSON = JSON.stringify(giphy);
+//var giphy = {gif:""};
+//var JSON = JSON.stringify(giphy);
 //window.location = "https://giphy.com/" + JSON;
 
-$("#search-button").click(function () {
-    var searchT = ("#search-term").val()
+$("#search-button").click(function() {
+    var searchT = $("#search-term").val()
     var newUrl=`https://api.giphy.com/v1/gifs/search?q=${searchT}&rating=pg&api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY`;
     fetch(newUrl)
-    .then(function () {
+    .then(function(response) {
             return response.json();
         })
-        .then(function () {
-            //let searchTerm  = giphy.gif;
-            let pic_url = data[0].show.image.medium;
-            $('body').append('<img src =' + searchTerm + pic_url + '/>');
-            let src = " ";
+        .then(function(data) {
+            let pic_url = data.data[0].images.downsized_large.url;
+            console.log(pic_url);
+            $('body').append('<img src =' + pic_url + '/>');
+            
 
         })
+
 
 
 });
